7fdbc3a6efe8fd6728e9b25630e13d525c05287c
ClaudiaGolino06 <80152181+ClaudiaGolino06@users.noreply.github.com>
2021-07-02 17:57:49 +0200

    testare

--- Files/DLX_vhd/a.b-datapath/registerfile.vhd
+++ Files/DLX_vhd/a.b-datapath/registerfile.vhd
@@ -2,8 +2,7 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.std_logic_unsigned.all;
 use IEEE.numeric_std.all;
-use WORK.all;
-use work.constants.all;
+use work.myTypes.all
 
 entity register_file is
  port ( CLK: 		IN std_logic;
@@ -22,43 +21,28 @@ end register_file;
 
 architecture A of register_file is
 
-        -- suggested structures
         subtype REG_ADDR is natural range 0 to Numreg-1; -- using natural type
 	type REG_ARRAY is array(REG_ADDR) of std_logic_vector(NumBit-1 downto 0); 
 	signal REGISTERS : REG_ARRAY; 
---	signal addwr: integer;
---	signal addrd1: integer;
---	signal addrd2: integer;
 	
 begin 
--- write your RF code 
 	process (CLK)
 	begin
-		if CLK'event and CLK='1' then--and enable='1' then
+		if CLK'event and CLK='1' then --and enable='1' then
 		if enable='1' then
 			if wr='1' then
-				for c in 0 to NumBit-1 loop
-					registers(to_integer(unsigned(add_wr)))(c)<=datain(c);
-				end loop;
+					registers(to_integer(unsigned(add_wr)))<=datain;
 			end if;
 			if rd1='1' then
-				for c in 0 to NumBit-1 loop
-					out1(c)<=registers(to_integer(unsigned(add_rd1)))(c);
-				end loop;
+					out1<=registers(to_integer(unsigned(add_rd1)));
 			end if;
 			if rd2='1' then
-				for c in 0 to NumBit-1 loop
-					out2(c)<=registers(to_integer(unsigned(add_rd2)))(c);
-				end loop;
+					out2<=registers(to_integer(unsigned(add_rd2)));
 			end if;
 		end if;
 		--if CLK'event and CLK='1' and reset='1' then
 		if reset='1' then
-			for c in 0 to NumBit-1 loop
-				for i in 0 to Numreg-1 loop
-					registers(i)(c)<='0';
-				end loop;	
-			end loop;	
+					registers<='0';	
 		end if;
 		end if;
 	end process;
