0103a0dae272a3aa896845ccbe7c2244fb2dd8dd
ClaudiaGolino06 <80152181+ClaudiaGolino06@users.noreply.github.com>
2021-07-02 16:56:56 +0200

    testare

--- Files/DLX_vhd/a.b-datapath/sign_ext.vhd
+++ Files/DLX_vhd/a.b-datapath/sign_ext.vhd
@@ -8,7 +8,7 @@ entity sign_eval is
            N_out: integer := N_BIT*2);
 	port (
 		IR_out: in std_logic_vector(N_in-1 downto 0);
-      negative: in std_logic; 
+      signed_val: in std_logic; 
       Immediate: out std_logic_vector (N_out-1 downto 0));
 end sign_eval;
 
@@ -17,6 +17,8 @@ architecture BHV of sign_eval is
 	constant ones : std_logic_vector(N_in-1 downto 0) := (others => '1');
 	
 	begin
-		immediate <= ones&IR_out when (negative = '1' and IR_out(N_in-1) = '1') else zeros&IR_out;
+		immediate <= ones&IR_out when (signed_val = '1' and IR_out(N_in-1) = '1') else zeros&IR_out;
 		
 end BHV;
+
+		
