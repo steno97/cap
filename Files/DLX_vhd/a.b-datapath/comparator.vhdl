43d4a86b7009ab28b6d2408ca2f974d4486fe672
steno97 <46044807+steno97@users.noreply.github.com>
2021-07-06 08:07:50 +0200

    Create comparator.vhdl

---
+++ Files/DLX_vhd/a.b-datapath/comparator.vhdl
@@ -0,0 +1,20 @@
+library IEEE;
+use IEEE.std_logic_1164.all; --  libreria IEEE con definizione tipi standard logic
+use work.myTypes.all;
+
+
+--devo fare il comparator
+entity ND2 is
+	Port (	A:	In	std_logic;
+		B:	In	std_logic;
+		Y:	Out	std_logic);
+end ND2;
+
+
+architecture ARCH1 of ND2 is
+
+begin
+	sommma a+b
+	if c
+
+end ARCH1;
